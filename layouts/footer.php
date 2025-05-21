684b4dcd5112c7c7ff6ef8c4be79b4bca7e10749
Alex Sander <alex.sdmg@hotmail.com>
2025-05-20 22:15:27 -0400

    Aula Prática 1

---
+++ layouts/footer.php
@@ -0,0 +1,57 @@
+<footer class="mt-5 py-5">
+        <div class="container">
+            <div class="row">
+            <!-- Coluna 1: Logo e frase -->
+            <div class="col-md-3 mb-4">
+                <img src="assets/imgs/logo.png" alt="Logo" class="logo-footer mb-2">
+                <p>Os melhores produtos pelo melhor preço.</p>
+            </div>
+
+            <!-- Coluna 2: Links principais -->
+            <div class="col-md-3 mb-4">
+                <h5>Destaque</h5>
+                <ul class="list-unstyled">
+                <li><a href="#" class="text-dark text-decoration-none">HOMENS</a></li>
+                <li><a href="#" class="text-dark text-decoration-none">MULHERES</a></li>
+                <li><a href="#" class="text-dark text-decoration-none">MENINOS</a></li>
+                <li><a href="#" class="text-dark text-decoration-none">MENINAS</a></li>
+                <li><a href="#" class="text-dark text-decoration-none">NOVIDADES</a></li>
+                <li><a href="#" class="text-dark text-decoration-none">CAMISETAS</a></li>
+                </ul>
+            </div>
+
+            <!-- Coluna 3: Contato -->
+            <div class="col-md-3 mb-4 contact-info">
+                <h5>Fale Conosco</h5>
+                <p>Telefone: (11) 1234-5678</p>
+                <p>Email: contato@loja.com</p>
+                <p>Endereço: Rua Exemplo, 123 - SP</p>
+            </div>
+
+            <!-- Coluna 4: Produtos em miniatura -->
+            <div class="col-md-3 mb-4">
+                <h5>Instagram</h5>
+                <div class="d-flex flex-wrap gap-2">
+                <img src="assets/imgs/1.jpeg" alt="Produto 1" class="thumb">
+                <img src="assets/imgs/2.jpeg" alt="Produto 2" class="thumb">
+                <img src="assets/imgs/3.jpeg" alt="Produto 3" class="thumb">
+                <img src="assets/imgs/4.jpeg" alt="Produto 4" class="thumb">
+                </div>
+            </div>
+            </div>
+
+            <!-- Linha inferior: Copyright -->
+            <div class="row border-top pt-3 mt-3">
+            <div class="col-md-6">
+                <p class="mb-0">eCommerce © 2022 All Right Reserved</p>
+            </div>
+            <div class="col-md-6 text-md-end">
+                <a href="#" class="text-light me-3"><i class="fab fa-facebook"></i></a>
+                <a href="#" class="text-light me-3"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="text-light"><i class="fab fa-twitter"></i></a>
+            </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ layouts/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="assets/imgs/logo.png" width="100px" />
+      </a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav-buttons">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Produtos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Blog</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link me-3" href="#">Fale Conosco</a>
+            </li>
+            <li class="nav-item d-flex align-items-center">
+                <i class="fa fa-shopping-cart me-3" aria-hidden="true"></i>
+            </li>
+            <li class="nav-item d-flex align-items-center">
+                <i class="fa fa-user" aria-hidden="true"></i>
+        </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
